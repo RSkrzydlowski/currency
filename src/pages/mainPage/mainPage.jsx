997bf43a5816176a8b258a6a87c1d3d14434700d
RSkrzydlowski <rafalskrzydlowski@gmail.com>
2019-11-21 14:38:10 +0100

    generate array with currency name

--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -4,9 +4,44 @@ import './mainPage.scss';
 class MainPage extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			value: []
+		};
+		this.urlB = 'http://api.nbp.pl/api/exchangerates/tables/B/';
+
+		this.init();
 	}
 
+	init = () => {
+		const value = [];
+		this.generateTable('A', value);
+		this.generateTable('B', value);
+		console.log(value);
+		// this.setState((prevState) => ({}));
+	};
+
+	generateTable = (letter, value) => {
+		const url = `http://api.nbp.pl/api/exchangerates/tables/${letter}/`;
+		fetch(url)
+			.then((res) => res.json())
+			.then((out) => {
+				// console.log(out);
+				for (let i = 0; i < out[0].rates.length; i++) {
+					value.push(out[0].rates[i].currency);
+					// this.setState((prevState) ;value.push(out[0].rates[i].currency)
+					// new Information(out[0].rates[i].code, tableInformation, letter);
+				}
+				// console.log(out[0].rates[0].currency);
+				// console.log(value);
+			})
+			.catch((err) => {
+				throw err;
+			});
+	};
+
 	render() {
+		console.log('FSD');
+		console.log(this.state.value);
 		return (
 			<div>
 				<div className="header">Currency</div>
